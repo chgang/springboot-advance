e2feff57221fb86c74667b1bfd9f46dd438a14db
chgang <chgang>
2019-09-07 15:00:52 +0800

    interviewer初始化

---
+++ interviewer/src/test/java/com/qskx/interviewer/InterviewerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.qskx.interviewer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InterviewerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
