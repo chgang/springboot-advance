155fa49b121c4063bc95a0d67aefc19eb53b2326
chgang <chgang>
2020-03-29 22:56:53 +0800

    指数取余

--- interviewer/src/test/java/com/qskx/interviewer/InterviewerApplicationTests.java
+++ interviewer/src/test/java/com/qskx/interviewer/InterviewerApplicationTests.java
@@ -1,16 +1,36 @@
 package com.qskx.interviewer;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
 public class InterviewerApplicationTests {
 
     @Test
     public void contextLoads() {
+        test1(90, 1000000007);
+        test2(90, 1000000007);
+    }
+
+    public void test1(int n, int p) {
+        long rem = 1, x = 3;
+        for (int a = n / 3 - 1; a > 0; a /= 2) {
+            if (a % 2 == 1) rem = (rem * x) % p;
+            x = (x * x) % p;
+        }
+        System.out.println(rem);
+    }
+
+    public void test2(int n, int p) {
+        int index;
+        if(n % 3 == 1) {
+            index = n/3 - 1;
+        } else {
+            index = n/3;
+        }
+        long remainder = 1;
+        for(int i = 0; i < index; i++) {
+            remainder = (3 * remainder) % 1000000007;
+        }
+        System.out.println(remainder);
     }
 
 }
